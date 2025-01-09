92a90b5f30a1b0a8708a7fa4b2f25ce063fccf13
Menna ayman <mennaaymangalal4@gmail.com>
2025-01-09 23:36:27 +0200

    upload

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import Linewhite from '../LineWhite/Linewhite'
+import hero from '../Assets/images/avataaars.aa9aff02ddd0ef36a1724ecef4133df9-_1_.png'
 
 
 export default function Hero() {
@@ -8,7 +9,7 @@ export default function Hero() {
     <>
     <div className="heroSection">
         <div className="d-flex flex-column justify-content-center align-items-center ">
-            <img className='w-25 pt-5' src="src/Hero/avataaars.aa9aff02ddd0ef36a1724ecef4133df9-_1_.png" alt="" />
+            <img className='w-25 pt-5' src={hero} alt="" />
             <h1 className='fw-bold text-white'>MY PORTFOLIO</h1>
            <Linewhite/>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/React-First-Project/', // Replace with your repository name
+  base: '/React-First-Project/', 
   build: {
     outDir: 'dist',
   },
